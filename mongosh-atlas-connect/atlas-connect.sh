b983cadaeba01a4fb3663f2697f63b3a97a99fce
bcrisologo <jblaise0912@gmail.com>
2021-10-14 11:26:01 -0700

    Updated auto-connect options

--- mongosh-atlas-connect/atlas-connect.sh
+++ mongosh-atlas-connect/atlas-connect.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
-mongosh "mongodb+srv://hostname.mongodb.net/collection" --username username
+# Prompt for URI
+echo "MongoDB URI: "
+read mongodb_uri
+
+# Prompt for MongoDB username
+echo "MongoDB username: "
+read mongodb_username
+
+mongosh "$mongodb_uri" --username $mongodb_username
+
+# Option if you want to the docker image to auto-connect to a MongoDB instance / Atlas cluster
+# mongosh "mongodb+srv://hostname.mongodb.net/collection" --username username
